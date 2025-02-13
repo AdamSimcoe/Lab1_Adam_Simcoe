905efd97b70f720e7e359a4ae5e047be2d1bd9ff
Adam Simcoe <adamsimcoe@Adams-Mac-mini.local>
2025-02-12 20:42:16 -0500

    added state vars forthe current number being displayed, and tracking guesses/attempts

--- Lab1_Adam_Simcoe/ContentView.swift
+++ Lab1_Adam_Simcoe/ContentView.swift
@@ -4,6 +4,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentNum = Int.random(in: 1...100)
+    @State private var correctGuesses = 0
+    @State private var wrongGuesses = 0
+    @State private var attempts = 0
+    
     var body: some View {
         VStack {
             
