b6ac945a0b7718de0e4ad28af8620627a802f4a4
Adam Simcoe <adamsimcoe@Adams-Mac-mini.local>
2025-02-12 20:22:20 -0500

    Initial commit

---
+++ Lab1_Adam_Simcoe/ContentView.swift
@@ -0,0 +1,13 @@
+// Created by Adam Simcoe - 101442161
+// Last Updated on February 12th, 2025
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
---
+++ Lab1_Adam_Simcoe/Lab1_Adam_SimcoeApp.swift
@@ -0,0 +1,13 @@
+// Created by Adam Simcoe - 101442161
+// Last Updated on February 12th, 2025
+
+import SwiftUI
+
+@main
+struct Lab1_Adam_SimcoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
